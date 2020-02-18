3073280d08c6fe331aa8fed5d3a5fd05980cf6fb
sachinp-git <svp.reaper94@gmail.com>
2020-02-18 12:42:26 +0530

    update employee test

--- src/employee.test.js
+++ src/employee.test.js
@@ -6,28 +6,28 @@ import {
   deleteEmployeeFunc
 } from "./test-functions/employee";
 
-// test('Add new employee', async ()=> {
+test('Add new employee', async ()=> {
 
-//     const data = {
-//         input: {
-//             firstname: "Sachin",
-//             lastname: "Pandey",
-//             address: [
-//                 {
-//                     line1: "A-2 604",
-//                     line2: "Ambrossia",
-//                     zipcode: "400615",
-//                     city: "thane",
-//                     state: "Maharashtra"
-//                 }
-//             ]
-//         }
-//     }
-//     const result = await addNewEmployeefunc(data);
-//     console.log(JSON.stringify(result,6));
-//     expect(result.data).not.toBeUndefined();
-//     expect(result.errors).toBeUndefined();
-// })
+    const data = {
+        input: {
+            firstname: "Sachin",
+            lastname: "Pandey",
+            address: [
+                {
+                    line1: "A-2 604",
+                    line2: "Ambrossia",
+                    zipcode: "400615",
+                    city: "thane",
+                    state: "Maharashtra"
+                }
+            ]
+        }
+    }
+    const result = await addNewEmployeefunc(data);
+    console.log(JSON.stringify(result,6));
+    expect(result.data).not.toBeUndefined();
+    expect(result.errors).toBeUndefined();
+})
 
 test('Add new employee with skill', async ()=> {
 
@@ -60,48 +60,48 @@ test('Add new employee with skill', async ()=> {
     expect(result.errors).toBeUndefined();
 })
 
-// test('list employees', async ()=> {
-//     const result = await listEmployeesfunc();
-//     console.log(JSON.stringify(result,6))
-//     expect(result.data).not.toBeUndefined();
-//     expect(result.data).not.toBeNull();
-//     expect(result.errors).toBeUndefined();
+test('list employees', async ()=> {
+    const result = await listEmployeesfunc();
+    console.log(JSON.stringify(result,6))
+    expect(result.data).not.toBeUndefined();
+    expect(result.data).not.toBeNull();
+    expect(result.errors).toBeUndefined();
 
-// })
+})
 
-// test('get existing employee by ID', async () => {
-//     const data = {
-//             id: "4e2b0841-8b83-4aa6-9b03-9ad02a65e462"
-//     }
-//     const result = await getEmployeefunc(data);
-//     console.log(JSON.stringify(result,6))
-//     expect(result.errors).toBeUndefined();
-//     expect(result.data).not.toBeUndefined();
-//     expect(result.data).not.toBeNull();
-// })
+test('get existing employee by ID', async () => {
+    const data = {
+            id: "4baccb0c-28ff-44a8-bd41-9922f6af4796"
+    }
+    const result = await getEmployeefunc(data);
+    console.log(JSON.stringify(result,6))
+    expect(result.errors).toBeUndefined();
+    expect(result.data).not.toBeUndefined();
+    expect(result.data).not.toBeNull();
+})
 
-// test("update existing employee", async () => {
-//   const data = {
-//     input: {
-//       id: "4e2b0841-8b83-4aa6-9b03-9ad02a65e462",
-//       lastname: "Moh",
-//       firstname: "Salah",
-//       address: [
-//         {
-//           id: "00129c06-7aad-4f6f-8937-82849a6c88c4",
-//           city: "Egypt"
-//         }
-//       ]
-//     }
-//   };
+test("update existing employee", async () => {
+  const data = {
+    input: {
+      id: "4baccb0c-28ff-44a8-bd41-9922f6af4796",
+      firstname: "Salah",
+      address: [
+        {
+          id: "ea10556e-adee-4480-b6f8-e332cabdd1f4",
+          city: "Egypt"
+        }
+      ]
+    }
+  };
 
-//   const result = await editEmployeefunc(data);
-//   console.log(JSON.stringify(result, 6));
-//   expect(result.errors).toBeUndefined();
-//   expect(result.data).not.toBeUndefined();
-//   expect(result.data).not.toBeNull();
-// });
+  const result = await editEmployeefunc(data);
+  console.log(JSON.stringify(result, 6));
+  expect(result.errors).toBeUndefined();
+  expect(result.data).not.toBeUndefined();
+  expect(result.data).not.toBeNull();
+});
 
+//Uncomment and add to id of an existing employee
 // test("delete existing employee", async () => {
 //   const data = {
 //     input: {
